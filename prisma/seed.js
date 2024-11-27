20926aac2eac621e7b6717b7c0a2cee92a893cde
Dams168 <adamrohman2003@gmail.com>
2024-11-27 21:08:31 +0700

    update:seeder & fix:schema

--- prisma/seed.js
+++ prisma/seed.js
@@ -1,10 +1,16 @@
 const { PrismaClient } = require('@prisma/client');
 const prisma = new PrismaClient();
 const generateSeats = require('../src/utils/generateSeats');
+const generateDuration = require('../src/utils/durationUtil');
 
 async function main() {
     try {
+        await prisma.seatClasses.deleteMany();
+        await prisma.routes.deleteMany();
         await prisma.airports.deleteMany();
+        await prisma.planes.deleteMany();
+        await prisma.seats.deleteMany();
+        await prisma.flights.deleteMany();
 
         const airports = await prisma.airports.createMany({
             data: [
@@ -75,8 +81,7 @@ async function main() {
             skipDuplicates: true,
         });
 
-        await prisma.planes.deleteMany();
-        await prisma.seats.deleteMany();
+
         const planesData = [
             {
                 planeName: 'Jet Air',
@@ -120,6 +125,101 @@ async function main() {
 
         }
 
+
+        await prisma.seatClasses.create({
+            data:
+            {
+                name: 'Economy',
+                priceAdult: 4950000,
+                priceChild: 4500000,
+                priceBaby: 0,
+                route: {
+                    create: {
+                        departureAirportId: 1,
+                        arrivalAirportId: 2,
+                    },
+                },
+            },
+            include: {
+                route: true,
+            },
+        });
+
+        await prisma.seatClasses.create({
+            data:
+            {
+                name: 'Business',
+                priceAdult: 9950000,
+                priceChild: 9500000,
+                priceBaby: 0,
+                route: {
+                    create: {
+                        departureAirportId: 1,
+                        arrivalAirportId: 4,
+                    },
+                },
+            },
+            include: {
+                route: true,
+            },
+        });
+
+        await prisma.seatClasses.create({
+            data:
+            {
+                name: 'First Class',
+                priceAdult: 14950000,
+                priceChild: 14500000,
+                priceBaby: 0,
+                route: {
+                    create: {
+                        departureAirportId: 1,
+                        arrivalAirportId: 6,
+                    },
+                },
+            },
+            include: {
+                route: true,
+            },
+        });
+
+        const duration1 = generateDuration('2024-12-01T08:00:00.000', '2024-12-01T11:30:00.000');
+
+        await prisma.flights.create({
+            data: {
+                departureTime: new Date('2024-12-01T08:00:00.000'),
+                arrivalTime: new Date('2024-12-01T11:30:00.000'),
+                planeId: 1,
+                routeId: 1,
+                duration: duration1,
+                flightCode: 'Flight-001',
+            },
+        });
+        const duration2 = generateDuration('2024-12-01T09:00:00.000', '2024-12-01T15:25:00.000');
+
+        await prisma.flights.create({
+            data: {
+                departureTime: new Date('2024-12-01T09:00:00.000'),
+                arrivalTime: new Date('2024-12-01T15:25:00.000'),
+                planeId: 2,
+                routeId: 2,
+                duration: duration2,
+                flightCode: 'Flight-002',
+            },
+        });
+
+        const duration3 = generateDuration('2024-12-02T07:00:00.000', '2024-12-03T00:35:00.000');
+
+        await prisma.flights.create({
+            data: {
+                departureTime: new Date('2024-12-02T07:00:00.000'),
+                arrivalTime: new Date('2024-12-03T00:35:00.000'),
+                planeId: 3,
+                routeId: 3,
+                duration: duration3,
+                flightCode: 'Flight-003',
+            },
+        });
 
         console.log(`Database has been seeded. 🌱`);
     } catch (error) {
