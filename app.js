7934d8ec171b12e518cec83ed33e83b454745f4b
Dams168 <adamrohman2003@gmail.com>
2024-11-28 12:52:15 +0700

    add: Create,Read, delete plane

--- app.js
+++ app.js
@@ -1,4 +1,4 @@
-if(process.env.NODE_ENV !== 'production'){
+if (process.env.NODE_ENV !== 'production') {
     require('dotenv').config();
 }
 const express = require('express')
@@ -6,12 +6,14 @@ const errorHandler = require('./src/middlewares/errorHandler');
 const app = express()
 const PORT = 3000
 const airportRoute = require('./src/routes/admin/airportRoute')
+const planeRoute = require('./src/routes/admin/planeRoute')
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
+app.use(planeRoute)
 app.use(airportRoute)
 app.use(errorHandler);
 
-app.listen (PORT, () => {
+app.listen(PORT, () => {
     console.log(`-> Listening on PORT: ${PORT}`);
 })
--- src/controllers/admin/airportController.js
+++ src/controllers/admin/airportController.js
@@ -1,4 +1,4 @@
-if(process.env.NODE_ENV !== 'production'){
+if (process.env.NODE_ENV !== 'production') {
     require('dotenv').config();
 }
 const { PrismaClient } = require('@prisma/client');
@@ -10,21 +10,21 @@ const supabase = createClient(supabaseUrl, supabaseKey)
 
 const addNewAirport = async (req, res, next) => {
     try {
-        let { name, city, country, continent, airportCode} = req.body;
-        if(typeof(name)!== 'string' || typeof(city)!== 'string' || typeof(country)!== 'string' || typeof(airportCode)!== 'string' || typeof(continent)!== 'string') {
-            return res.status(400).json({message: "Invalid input data"});
+        let { name, city, country, continent, airportCode } = req.body;
+        if (typeof (name) !== 'string' || typeof (city) !== 'string' || typeof (country) !== 'string' || typeof (airportCode) !== 'string' || typeof (continent) !== 'string') {
+            return res.status(400).json({ message: "Invalid input data" });
         }
         name = name.trim();
         city = city.trim();
         country = country.trim();
         airportCode = airportCode.trim();
         continent = continent.trim();
-        if (!name || !city || !country || !airportCode ||!continent) {
+        if (!name || !city || !country || !airportCode || !continent) {
             return res.status(400).send('Please provide all required fields');
         }
-        const {data, error} = await supabase
+        const { data, error } = await supabase
             .from('Airports')
-            .insert([{ name, city, country, continent, airportCode}])
+            .insert([{ name, city, country, continent, airportCode }])
             .single();
         if (error) {
             console.error('Supabase Insert Error:', error.message);
@@ -34,13 +34,13 @@ const addNewAirport = async (req, res, next) => {
             message: 'Airport added successfully.',
         });
     } catch (error) {
-        return res.status(500).json({message: "Error adding new airport"});
+        return res.status(500).json({ message: "Error adding new airport" });
     }
 }
 
 const getAllAirports = async (req, res, next) => {
     try {
-        const {data, error} = await supabase
+        const { data, error } = await supabase
             .from('Airports')
             .select('*');
         if (error) {
@@ -49,7 +49,7 @@ const getAllAirports = async (req, res, next) => {
         }
         return res.status(200).json({ data });
     } catch (error) {
-        return res.status(500).json({message: "Error fetching airports"});
+        return res.status(500).json({ message: "Error fetching airports" });
     }
 }
 
@@ -78,7 +78,7 @@ const deleteAirport = async (req, res, next) => {
         if (deleteError) {
             return res.status(500).json({ message: 'Failed to delete airport.', error: deleteError.message });
         }
-        return res.status(200).json({ message: 'Airport deleted successfully.'});
+        return res.status(200).json({ message: 'Airport deleted successfully.' });
     } catch (err) {
         console.error('Unhandled Error:', err);
         return res.status(500).json({ message: 'Error deleting airport.' });
@@ -90,19 +90,19 @@ const addMultipleAirports = async (req, res, next) => {
         const airports = req.body;
         for (const [index, airport] of airports.entries()) {
             const { name, city, country, continent, airportCode } = airport;
-        
+
             if (![name, city, country, continent, airportCode].every(field => typeof field === 'string' && field.trim() !== '')) {
                 return res.status(400).json({
                     message: `Invalid input data at index ${index}. Each airport must have non-empty string fields.`,
                     invalidData: airport,
                 });
             }
-        }        
+        }
 
         const { data, error } = await supabase
             .from('Airports')
-            .insert(airports) 
-            .select('*'); 
+            .insert(airports)
+            .select('*');
 
         if (error) {
             console.error('Supabase Insert Error:', error.message);
@@ -126,4 +126,4 @@ const addMultipleAirports = async (req, res, next) => {
 };
 
 
-module.exports = {addNewAirport, deleteAirport, getAllAirports, addMultipleAirports};
+module.exports = { addNewAirport, deleteAirport, getAllAirports, addMultipleAirports };
