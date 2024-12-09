a40292e4b5056e17d2bdc28cbd1f7b753d66aba6
Dams168 <adamrohman2003@gmail.com>
2024-12-09 21:05:09 +0700

    add: update-password update:statusCode

--- src/routes/user/resetPasswordRoute.js
+++ src/routes/user/resetPasswordRoute.js
@@ -1,8 +1,12 @@
 const router = require('express').Router();
 const resetPasswordController = require('../../controllers/user/resetPasswordController');
+const authMiddleware = require('../../middlewares/authMiddleware');
 
 router.post('/verify-email', resetPasswordController.verifyEmail);
 router.post('/reset-password/:token', resetPasswordController.resetPassword);
 router.get('/verify-token/:token', resetPasswordController.verifyToken);
+
+//update password
+router.post('/update-password/:id', authMiddleware, resetPasswordController.updatePassword);
 
 module.exports = router;
