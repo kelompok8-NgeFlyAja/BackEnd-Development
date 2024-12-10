c1d2ad515e72901fd40f50877d94372af46b44d9
aldyrmdhns <aldyramadhans101@gmail.com>
2024-12-10 19:35:06 +0800

    Update and Fix Payment, Add Expiry Time

--- src/controllers/admin/resetSeatController.js
+++ src/controllers/admin/resetSeatController.js
@@ -6,6 +6,17 @@ const resetSeat = async (req, res, next) => {
 		const { planeId } = req.params;
 		const convertPlaneId = parseInt(planeId);
 
+		// const seatInfo = await prisma.seats.findMany({
+		// 	where: { planeId: convertPlaneId },
+		// 	select: { id: true },
+		// });
+
+		// if (seatInfo.length === 0) {
+		// 	const error = new Error("No Seats Found")
+		// 	error.statusCode(404)
+		// 	throw error
+		// }
+
 		if (isNaN(convertPlaneId)) {
 			const error = new Error("Invalid plane ID");
 			error.statusCode = 400;
@@ -16,6 +27,13 @@ const resetSeat = async (req, res, next) => {
 			where: { planeId: convertPlaneId },
 			data: { isAvailable: true },
 		});
+
+		// const resetPassengers = await prisma.passengers.updateMany({
+		// 	where: {
+		// 		seatId: { in: seatInfo.map(seat => seat.id) },
+		// 	},
+		// 	data: { seatId: null },
+		// });
 
 		if (updatedSeats.count > 0) {
 			return res.status(200).json({
