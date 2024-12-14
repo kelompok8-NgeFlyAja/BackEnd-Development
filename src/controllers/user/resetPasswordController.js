6d7fdd4dddb662eb2a840c74d7655056e6300be7
Dams168 <adamrohman2003@gmail.com>
2024-12-15 01:53:40 +0700

    update: url reset password

--- src/controllers/user/resetPasswordController.js
+++ src/controllers/user/resetPasswordController.js
@@ -32,7 +32,7 @@ const verifyEmail = async (req, res, next) => {
                 }
             });
 
-            const resetUrl = `http://ngeflyaja.shop/verify-token/${token}`;
+            const resetUrl = `http://localhost:5173/verify-token/${token}`;
 
             const mailOptions = {
                 from: '"NgeFlyAja" <no-reply@ngeflyaja.com>',
