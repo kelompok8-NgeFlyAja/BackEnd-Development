936acf1e544dd28fa3cbb54469019a3c170c0df0
Dams168 <adamrohman2003@gmail.com>
2024-12-22 14:21:00 +0700

    add:return-search-flight fix:search-flight

--- src/controllers/user/search.js
+++ src/controllers/user/search.js
@@ -4,12 +4,17 @@ const moment = require("moment-timezone");
 
 const searchFlights = async (req, res, next) => {
     try {
-        const { departureAirportCode, arrivalAirportCode, departureTime, seatClasses, adultPassenger, childPassenger, babyPassenger, returnTime = 'null', page = 1, pageSize = 10 } = req.query;
-        // let returnTime = req.query.returnTime || null;
-        console.log({ departureAirportCode, arrivalAirportCode, departureTime, returnTime, seatClasses, adultPassenger, childPassenger, babyPassenger, returnTime, page, pageSize });
-        // if (!returnTime) {
-        //     returnTime = null;
-        // }
+        const {
+            departureAirportCode,
+            arrivalAirportCode,
+            departureTime,
+            seatClasses,
+            adultPassenger,
+            childPassenger,
+            babyPassenger,
+            page = 1,
+            pageSize = 10
+        } = req.query;
 
         if (!departureAirportCode || !arrivalAirportCode || !departureTime || !seatClasses || !adultPassenger || !childPassenger || !babyPassenger) {
             const error = new Error("Please provide all required fields");
@@ -28,21 +33,22 @@ const searchFlights = async (req, res, next) => {
         const seatClassesLower = seatClasses.toLowerCase();
         const totalPassengers = parseInt(adultPassenger) + parseInt(childPassenger) + parseInt(babyPassenger);
 
-        if(departureAirportCodeLower === arrivalAirportCodeLower) {
+        if (departureAirportCodeLower === arrivalAirportCodeLower) {
             const error = new Error("Departure and arrival airport cannot be the same");
             error.status = 400;
+            throw error;
+        }
+
+        const isValidDate = moment(departureTime, "YYYY-MM-DD", true).isValid();
+        if (!isValidDate) {
+            const error = new Error("Invalid date format or non-existent date");
+            error.statusCode = 400;
             throw error;
         }
 
         const parsedDate = new Date(departureTime);
         if (isNaN(parsedDate)) {
             const error = new Error("Invalid date format");
-            error.statusCode = 400;
-			throw error;
-        }
-
-        if (parsedDate < new Date()) {
-            const error = new Error("Invalid date");
             error.statusCode = 400;
             throw error;
         }
@@ -55,8 +61,6 @@ const searchFlights = async (req, res, next) => {
                 }
             }
         });
-
-        // console.log('departureAirport', departureAirport);
 
         if (!departureAirport || departureAirport.length === 0) {
             const error = new Error("Airport not found");
@@ -73,8 +77,6 @@ const searchFlights = async (req, res, next) => {
             }
         });
 
-        // console.log('arrivalAirport', arrivalAirport);
-
         if (!arrivalAirport || arrivalAirport.length === 0) {
             const error = new Error("Airport not found");
             error.status = 404;
@@ -82,7 +84,7 @@ const searchFlights = async (req, res, next) => {
         }
 
         const existingRoute = await prisma.routes.findMany({
-            where:{
+            where: {
                 departureAirportId: departureAirport[0].id,
                 arrivalAirportId: arrivalAirport[0].id,
             }
@@ -92,8 +94,6 @@ const searchFlights = async (req, res, next) => {
             error.statusCode = 404;
             throw error;
         }
-
-        // console.log("Existing Route:", existingRoute);
 
         const pageNumber = parseInt(page);
         const itemsPerPage = parseInt(pageSize);
@@ -111,8 +111,8 @@ const searchFlights = async (req, res, next) => {
                     },
                 },
                 departureTime: {
-                    gte: new Date(parsedDate.setHours(0, 0, 0)), // Mulai hari
-                    lt: new Date(parsedDate.setHours(23, 59, 59)), // Akhir hari
+                    gte: new Date(parsedDate.setHours(0, 0, 0)),
+                    lt: new Date(parsedDate.setHours(23, 59, 59)),
                 },
             },
             include: {
@@ -131,17 +131,14 @@ const searchFlights = async (req, res, next) => {
             take: itemsPerPage
         });
 
-        // console.log("Flights:", flights);
-        
         const availableFlights = flights.filter((flight) => {
             const availableSeats = flight.plane.seats.filter((seat) => seat.isAvailable).length;
-            // console.log(`Seat tersedia: ${availableSeats}`);
             return availableSeats >= parseInt(totalPassengers);
         });
-        
+
         if (availableFlights.length === 0) {
             const error = new Error("No flights available for the given criteria");
-            error.statusCode = 404;
+            error.status = 404;
             throw error;
         }
 
@@ -186,58 +183,6 @@ const searchFlights = async (req, res, next) => {
             };
         });
 
-        const availableReturnFlights = flights.filter((flight) => {
-            const availableSeats = flight.plane.seats.filter((seat) => seat.isAvailable).length;
-            // console.log(`Seat return tersedia: ${availableSeats}`);
-            return availableSeats >= totalPassengers;
-        });
-
-        if (availableReturnFlights.length === 0) {
-            const error = new Error("No return flights available for the given criteria");
-            error.status = 404;
-            throw error;
-        }
-
-        const availableReturnFlightsResponse = availableReturnFlights.map((flight) => {
-            const timeZone = "Asia/Jakarta";
-            const returnTimeConvert = moment
-                .utc(flight.returnTime)
-                .tz(timeZone)
-                .format("YYYY-MM-DD HH:mm:ss");
-            const arrivalTimeConvert = moment
-                .utc(flight.arrivalTime)
-                .tz(timeZone)
-                .format("YYYY-MM-DD HH:mm:ss");
-
-            const convertreturnTimeToDate = new Date(returnTimeConvert);
-            const convertArrivalTimeToDate = new Date(arrivalTimeConvert);
-            return {
-                flightId: flight.id,
-                departureAirport: departureAirportCode,
-                arrivalAirport: arrivalAirportCode,
-                departureTime: convertreturnTimeToDate.toLocaleTimeString(),
-                departureDate: convertreturnTimeToDate.toLocaleDateString(),
-                arrivalTime: convertArrivalTimeToDate.toLocaleTimeString(),
-                arrivalDate: convertArrivalTimeToDate.toLocaleDateString(),
-                flightCode: flight.flightCode,
-                duration: flight.duration,
-                route: {
-                    routeId: flight.route.id,
-                    departureAirport: departureAirportCode,
-                    arrivalAirport: arrivalAirportCode,
-                    seatClass: seatClasses,
-                },
-                plane: {
-                    planeId: flight.plane.id,
-                    planeName: flight.plane.name,
-                    planeCode: flight.plane.planeCode,
-                    description: flight.plane.description,
-                    baggage: flight.plane.baggage,
-                    cabinBaggage: flight.plane.cabinBaggage,
-                },
-            };
-        });
-
         const totalFlights = await prisma.flights.count({
             where: {
                 route: {
@@ -260,11 +205,219 @@ const searchFlights = async (req, res, next) => {
             page: pageNumber,
             totalFlights: totalFlights,
             flights: availableFlightsResponse,
-            returnFlights: availableReturnFlightsResponse,
         });
     } catch (error) {
         next(error);
     }
 };
 
-module.exports = { searchFlights };
+const returnSearchFlights = async (req, res, next) => {
+    try {
+        const {
+            departureAirportCode,
+            arrivalAirportCode,
+            returnTime,
+            seatClasses,
+            adultPassenger,
+            childPassenger,
+            babyPassenger,
+            page = 1,
+            pageSize = 10
+        } = req.query;
+
+        if (!departureAirportCode || !arrivalAirportCode || !returnTime || !seatClasses || !adultPassenger || !childPassenger || !babyPassenger) {
+            const error = new Error("Please provide all required fields");
+            error.status = 400;
+            throw error;
+        }
+
+        if (typeof departureAirportCode !== 'string' || typeof arrivalAirportCode !== 'string' || typeof seatClasses !== "string" || isNaN(adultPassenger) || isNaN(childPassenger) || isNaN(babyPassenger)) {
+            const error = new Error("Invalid input data");
+            error.status = 400;
+            throw error;
+        }
+
+        const departureAirportCodeLower = departureAirportCode.toLowerCase();
+        const arrivalAirportCodeLower = arrivalAirportCode.toLowerCase();
+        const seatClassesLower = seatClasses.toLowerCase();
+        const totalPassengers = parseInt(adultPassenger) + parseInt(childPassenger) + parseInt(babyPassenger);
+
+        if (departureAirportCodeLower === arrivalAirportCodeLower) {
+            const error = new Error("Departure and arrival airport cannot be the same");
+            error.status = 400;
+            throw error;
+        }
+
+        const isValidReturnDate = moment(returnTime, "YYYY-MM-DD", true).isValid();
+        if (!isValidReturnDate) {
+            const error = new Error("Invalid return date format or non-existent date");
+            error.statusCode = 400;
+            throw error;
+        }
+
+        const parsedReturnDate = new Date(returnTime);
+        if (isNaN(parsedReturnDate)) {
+            const error = new Error("Invalid return date format");
+            error.statusCode = 400;
+            throw error;
+        }
+
+        const arrivalAirport = await prisma.airports.findMany({
+            where: {
+                airportCode: {
+                    equals: arrivalAirportCodeLower,
+                    mode: 'insensitive',
+                }
+            }
+        });
+
+        if (!arrivalAirport || arrivalAirport.length === 0) {
+            const error = new Error("Airport not found");
+            error.status = 404;
+            throw error;
+        }
+
+        const departureAirport = await prisma.airports.findMany({
+            where: {
+                airportCode: {
+                    equals: departureAirportCodeLower,
+                    mode: 'insensitive',
+                }
+            }
+        });
+
+        if (!departureAirport || departureAirport.length === 0) {
+            const error = new Error("Airport not found");
+            error.status = 404;
+            throw error;
+        }
+
+        const returnRoute = await prisma.routes.findMany({
+            where: {
+                departureAirportId: arrivalAirport[0].id,
+                arrivalAirportId: departureAirport[0].id,
+            }
+        });
+        if (!returnRoute || returnRoute.length === 0) {
+            const error = new Error("Return route not found");
+            error.statusCode = 404;
+            throw error;
+        }
+
+        const pageNumber = parseInt(page);
+        const itemsPerPage = parseInt(pageSize);
+        const offset = (pageNumber - 1) * itemsPerPage;
+        const returnFlights = await prisma.flights.findMany({
+            where: {
+                route: {
+                    departureAirportId: arrivalAirport[0].id,
+                    arrivalAirportId: departureAirport[0].id,
+                    seatClass: {
+                        name: {
+                            equals: seatClassesLower,
+                            mode: 'insensitive',
+                        },
+                    },
+                },
+                departureTime: {
+                    gte: new Date(parsedReturnDate.setHours(0, 0, 0)),
+                    lt: new Date(parsedReturnDate.setHours(23, 59, 59)),
+                },
+            },
+            include: {
+                route: {
+                    include: {
+                        seatClass: true,
+                    },
+                },
+                plane: {
+                    include: {
+                        seats: true,
+                    },
+                },
+            },
+            skip: offset,
+            take: itemsPerPage
+        });
+
+        const availableReturnFlights = returnFlights.filter((flight) => {
+            const availableSeats = flight.plane.seats.filter((seat) => seat.isAvailable).length;
+            return availableSeats >= parseInt(totalPassengers);
+        });
+
+        if (availableReturnFlights.length === 0) {
+            const error = new Error("No return flights available for the given criteria");
+            error.status = 404;
+            throw error;
+        }
+
+        const availableReturnFlightsResponse = availableReturnFlights.map((flight) => {
+            const timeZone = "Asia/Jakarta";
+            const departureTimeConvert = moment
+                .utc(flight.departureTime)
+                .tz(timeZone)
+                .format("YYYY-MM-DD HH:mm:ss");
+            const arrivalTimeConvert = moment
+                .utc(flight.arrivalTime)
+                .tz(timeZone)
+                .format("YYYY-MM-DD HH:mm:ss");
+
+            const convertDepartureTimeToDate = new Date(departureTimeConvert);
+            const convertArrivalTimeToDate = new Date(arrivalTimeConvert);
+            return {
+                flightId: flight.id,
+                departureAirport: arrivalAirportCode,
+                arrivalAirport: departureAirportCode,
+                departureTime: convertDepartureTimeToDate.toLocaleTimeString(),
+                departureDate: convertDepartureTimeToDate.toLocaleDateString(),
+                arrivalTime: convertArrivalTimeToDate.toLocaleTimeString(),
+                arrivalDate: convertArrivalTimeToDate.toLocaleDateString(),
+                flightCode: flight.flightCode,
+                duration: flight.duration,
+                route: {
+                    routeId: flight.route.id,
+                    departureAirport: arrivalAirportCode,
+                    arrivalAirport: departureAirportCode,
+                    seatClass: seatClasses,
+                },
+                plane: {
+                    planeId: flight.plane.id,
+                    planeName: flight.plane.name,
+                    planeCode: flight.plane.planeCode,
+                    description: flight.plane.description,
+                    baggage: flight.plane.baggage,
+                    cabinBaggage: flight.plane.cabinBaggage,
+                },
+                price: flight.route.seatClass.priceAdult
+            };
+        });
+
+        const totalReturnFlights = await prisma.flights.count({
+            where: {
+                route: {
+                    departureAirportId: arrivalAirport[0].id,
+                    arrivalAirportId: departureAirport[0].id,
+                    seatClass: { name: seatClasses, },
+                },
+                departureTime: {
+                    gte: new Date(parsedReturnDate.setHours(0, 0, 0)),
+                    lt: new Date(parsedReturnDate.setHours(23, 59, 59)),
+                },
+            },
+        });
+
+        return res.status(200).json({
+            status: "success",
+            statusCode: 200,
+            message: "Return flights retrieved successfully",
+            totalPages: Math.ceil(totalReturnFlights / itemsPerPage),
+            page: pageNumber,
+            totalReturnFlights: totalReturnFlights,
+            flights: availableReturnFlightsResponse,
+        });
+    } catch (error) {
+        next(error);
+    }
+};
+
+module.exports = { searchFlights, returnSearchFlights };
--- src/routes/user/searchRoute.js
+++ src/routes/user/searchRoute.js
@@ -3,5 +3,6 @@ const router = express.Router();
 const search = require('../../controllers/user/search');
 
 router.get('/search-flights', search.searchFlights);
+router.get('/return-search-flights', search.returnSearchFlights);
 
 module.exports = router;
